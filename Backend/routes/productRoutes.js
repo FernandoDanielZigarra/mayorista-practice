f0b0d18fe7af4b0b136ceeae7aeaeb3c1aaf08e9
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2023-10-17 16:10:42 -0300

    feat:  filter by category done

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {upload} = require('../middlewares/uploadMiddleware');
+const { upload } = require('../middlewares/uploadMiddleware');
 const validateProductData = require('../middlewares/validationMiddleware');
-const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct, getProductsByCategory } = require('../controllers/productController');
 
 router.get('/products', getAllProducts);
 router.get('/products/:id', getProductById);
 
-router.post('/products', upload.single('image'), validateProductData,createProduct);
+// Ruta para obtener productos por categoría
+router.get('/products/categoria/:categoriaID', getProductsByCategory);
 
+router.post('/products', upload.single('image'), validateProductData, createProduct);
 router.put('/products/:id', updateProduct);
 router.delete('/products/:id', deleteProduct);
 
