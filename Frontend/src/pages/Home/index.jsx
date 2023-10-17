84f244590ab121edc8ff04ac471387aa4f903513
unknown <fernando.zigarra@gmail.com>
2023-10-17 16:51:37 -0300

    fix header/footer/carrousel

--- Frontend/src/pages/Home/index.jsx
+++ Frontend/src/pages/Home/index.jsx
@@ -7,16 +7,14 @@ function Home() {
   const offersHome = data ? data.sort((a,b) => b.discount - a.discount).slice(0, 6) : [];
 
   return (
-    <div className="flex flex-col bg-slate-700 rounded-none">
+    <main className="flex flex-col bg-slate-700 rounded-none w-full">
       <div className="flex items-center justify-center flex-col gap-1 mt-3">
         <img src="https://i.pinimg.com/1200x/ec/d9/c2/ecd9c2e8ed0dbbc96ac472a965e4afda.jpg" alt="" className="w-16 h-16 rounded-[50%]" />
         <span className="text-xl text-white">Empresa</span>
       </div>
-      <h1 className="text-3xl w-full text-center my-5 text-slate-100 underline tracking-wide">Ofertas de la semana</h1>
-      <div className="rounded-t-3xl pt-5 w-screen shadow-[inset_0px_4px_13px_4px_#00000024] bg-slate-100 pb-5">
-        <section className="">
+      <h1 className="text-3xl text-center my-5 text-slate-100 underline tracking-wide">Ofertas de la semana</h1>
+      <div className="rounded-t-3xl pt-5 w-full shadow-[inset_0px_4px_13px_4px_#00000024] bg-slate-100 pb-5">
           <Carrousel />
-        </section>
         <h1 className="text-3xl w-full text-center my-5 text-black underline tracking-wide">Productos destacados</h1>
         <section className="w-[90%] mx-auto h-auto flex flex-wrap justify-evenly gap-y-3">
           {offersHome?.map((product) => (
@@ -24,7 +22,7 @@ function Home() {
           ))}
         </section>
       </div>
-    </div>
+    </main>
   );
 }
 
