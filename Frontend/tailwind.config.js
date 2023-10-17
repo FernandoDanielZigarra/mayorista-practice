1e70a10b02dc3cd802026aa47a66f46653c46255
unknown <fernando.zigarra@gmail.com>
2023-10-17 16:16:15 -0300

    add color

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,8 +3,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      flex:{
-        '2': '1 0 150px',
+      colors: {
+        'mariner': {
+          '50': '#f0f7fe',
+          '100': '#deecfb',
+          '200': '#c4e0f9',
+          '300': '#9ccbf4',
+          '400': '#6cafee',
+          '500': '#4a91e7',
+          '600': '#3574db',
+          '700': '#2d62cd',
+          '800': '#2a4ea3',
+          '900': '#274481',
+          '950': '#1c2c4f',
+        },
       }
     },
   },
