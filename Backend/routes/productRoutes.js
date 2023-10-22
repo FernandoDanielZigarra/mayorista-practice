e40d00dab72fbd193c3674e6a23bc8873722ee52
unknown <fernando.zigarra@gmail.com>
2023-10-21 23:59:15 -0300

    change product-card

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -6,15 +6,7 @@ const { getAllProducts, getProductById, createProduct, updateProduct, deleteProd
 
 router.get('/products', getAllProducts);
 router.get('/products/:id', getProductById);
-<<<<<<< HEAD
-router.post('/products', upload.single('image'), validateProductData, createProduct);
-=======
-
-
-
-
 router.post('/products', upload.array('images', '5'), validateProductData, createProduct);
->>>>>>> 92470d05b7359c36bd6ba58ac093d1d0a2d7229b
 router.put('/products/:id', updateProduct);
 router.delete('/products/:id', deleteProduct);
 
