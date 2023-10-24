12a84da618037e45f035ab4312853c062ab98ad1
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2023-10-24 14:30:16 -0300

    feat: update model product and controllers done

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { upload } = require('../middlewares/uploadMiddleware');
 const validateProductData = require('../middlewares/validationMiddleware');
-const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct,deleteAllProducts } = require('../controllers/productController');
 
 router.get('/products', getAllProducts);
 router.get('/products/:id', getProductById);
 router.post('/products', upload.array('images', '5'), validateProductData, createProduct);
 router.put('/products/:id', updateProduct);
+router.delete('/products/deleteAll',deleteAllProducts)
 router.delete('/products/:id', deleteProduct);
-
 module.exports = router;
