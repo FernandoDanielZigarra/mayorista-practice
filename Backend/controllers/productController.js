d0f7c0b1aaf79307fb0db3de815ab7c4da970d4e
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2023-10-29 20:44:56 -0300

    feat: update controller  done and contact

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -15,7 +15,7 @@ module.exports = {
       res.status(500).json({ message: error.message });
     }
   },
- 
+
   getProductById: async (req, res) => {
     try {
       const product = await Product.findById(req.params.id).populate("images", "urlImage").populate("category_id", "name").select('-__v');
@@ -26,21 +26,21 @@ module.exports = {
   },
   createProduct: async (req, res) => {
     try {
-      const { name, description, discount, price,fair_prices,price_per_unit,EAN,type,category_id } = req.body;
+      const { name, description, discount, price, fair_prices, price_per_unit, EAN, type, category_id } = req.body;
       const imageDefault = "https://firebasestorage.googleapis.com/v0/b/react-firebase-example-66176.appspot.com/o/product%2Fproduct-image-default.jpg?alt=media&token=90e8c25b-f59b-4142-b424-2f332f2b47a0&_gl=1*129ue9z*_ga*MTQzNjkxNDYyMi4xNjk0MDkzODkx*_ga_CW55HF8NVT*MTY5NzA1NDkxNS4yOC4xLjE2OTcwNTcxNjguNjAuMC4w"
-  
+
       const images = [];
-  
+
       if (req.files) {
         for (const file of req.files) {
           const fileName = `${"Product" + '-' + Date.now() + path.extname(file.originalname)}`;
           const uploadImage = await uploadFile(file, "product", fileName);
-  
+
           const newImage = new Image({
             filename: fileName,
             urlImage: uploadImage,
           });
-  
+
           await newImage.save();
           images.push(newImage._id);
         }
@@ -49,11 +49,11 @@ module.exports = {
           filename: "product-image-default.jpg",
           urlImage: imageDefault,
         });
-        
+
         await defaultImage.save();
         images.push(defaultImage._id);
       }
-  
+
       const product = new Product({
         name,
         description,
@@ -64,27 +64,43 @@ module.exports = {
         EAN,
         type,
         category_id,
-        images, // Asociamos el arreglo de imágenes al producto
+        images,
       });
-  
+
       await product.save();
       res.json(product);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   },
-  
-
 
   updateProduct: async (req, res) => {
     try {
-      const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-      res.json(product);
+      const { name, description, discount, price, fair_prices, price_per_unit, EAN, type, category_id } = req.body;
+      const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+        name,
+        description,
+        discount,
+        price,
+        fair_prices,
+        price_per_unit,
+        EAN,
+        type,
+        category_id
+      }, { new: true });
+
+      if (updatedProduct) {
+        res.json({
+          message: 'Producto actualizado correctamente',
+          product: updatedProduct
+        });
+      } else {
+        res.status(404).json({ message: 'No se encontró el producto' });
+      }
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   },
-
   deleteProduct: async (req, res) => {
     try {
       const product = await Product.findByIdAndDelete(req.params.id);
@@ -100,6 +116,6 @@ module.exports = {
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
-}
+  }
 }
 
